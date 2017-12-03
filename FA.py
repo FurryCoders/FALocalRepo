fe64d71c1f9efe87931f970e3855bbecb7318108
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2017-12-03 19:32:47 +0100

    Moved user input block outside of try

--- FA.py
+++ FA.py
@@ -3,26 +3,26 @@ import re
 import FA_DL as fadl
 import FA_DB as fadb
 
+users = input('Insert username: ')
+users = re.sub('( ){2,}', ' ', users.strip())
+users = re.sub('([^a-zA-Z0-9\-. ])', '', users.strip())
+users = users.split(' ')
+
+sections = input('Insert sections: ')
+sections = [s for s in sections if s in ('g','s','f','e','E')]
+
+speed = 1 ; update = False ; sync = False
+for o in input('Insert options: '):
+    if o == 'Q': speed = 2
+    elif o == 'S': speed = 0
+    elif o == 'U': update = True
+    elif o == 'Y': sync = True
+
+if not update and (len(users) == 0 or len(sections) == 0): exit(1)
+
+print()
+
 try:
-    users = input('Insert username: ')
-    users = re.sub('( ){2,}', ' ', users.strip())
-    users = re.sub('([^a-zA-Z0-9\-. ])', '', users.strip())
-    users = users.split(' ')
-
-    sections = input('Insert sections: ')
-    sections = [s for s in sections if s in ('g','s','f','e','E')]
-
-    speed = 1 ; update = False ; sync = False
-    for o in input('Insert options: '):
-        if o == 'Q': speed = 2
-        elif o == 'S': speed = 0
-        elif o == 'U': update = True
-        elif o == 'Y': sync = True
-
-    if not update and (len(users) == 0 or len(sections) == 0): exit(1)
-
-    print()
-
     try: Session = fadl.make_session()
     except FileNotFoundError: exit(1)
     if not fadl.check_cookies(Session): exit(2)
