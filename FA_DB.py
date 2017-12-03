9f9677e2779eb6fd7366aa33da92b75dbcce5c16
Matteo Campinoti <matteo.campinoti94@gmail.com>
2017-12-03 20:07:35 +0100

    Change blocks order

--- FA_DB.py
+++ FA_DB.py
@@ -1,19 +1,5 @@
 import sqlite3
 
-def db_usr_up(DB, user, to_add, column):
-    col = DB.execute(f"SELECT {column} FROM users WHERE name = '{user}'")
-    col = col.fetchall()[0]
-    col = "".join(col).split(',')
-    if to_add in col: return 1
-    if col[0] == '':
-        col = [to_add]
-    else:
-        col.append(to_add)
-    col.sort(key=str.lower)
-    col = ",".join(col)
-    DB.execute(f"UPDATE users SET {column} = '{col}' WHERE name = '{user}'")
-    DB.commit()
-
 def db_ins_usr(DB, user):
     try:
         DB.execute(f'''INSERT INTO USERS
@@ -36,6 +22,20 @@ def db_ins_sub(DB, infos):
     except:
         raise
 
+def db_usr_up(DB, user, to_add, column):
+    col = DB.execute(f"SELECT {column} FROM users WHERE name = '{user}'")
+    col = col.fetchall()[0]
+    col = "".join(col).split(',')
+    if to_add in col: return 1
+    if col[0] == '':
+        col = [to_add]
+    else:
+        col.append(to_add)
+    col.sort(key=str.lower)
+    col = ",".join(col)
+    DB.execute(f"UPDATE users SET {column} = '{col}' WHERE name = '{user}'")
+    DB.commit()
+        
 def db_usr_rep(DB, user, find, replace, column):
     col = DB.execute(f"SELECT {column} FROM users WHERE name = '{user}'")
     col = col.fetchall()[0]
