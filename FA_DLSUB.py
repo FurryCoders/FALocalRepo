f4870b288cc4fbee084e1197df21a09afe75ab5d
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2017-12-05 17:09:19 +0100

    Change check success return to true

--- FA_DLSUB.py
+++ FA_DLSUB.py
@@ -115,7 +115,7 @@ def str_clean(string):
 def dl_sub(Session, ID, folder, DB, quiet=False, check=False, speed=1):
     if check:
         if os.path.isfile(folder+'/info.txt'):
-            return 1
+            return True
 
     page = get_page(Session, ID)
     data = get_info(page) ; data.append(ID)
