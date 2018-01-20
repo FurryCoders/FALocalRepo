eab7a16d8a9cad5ef813102bf09ead572b33547b
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-01-20 15:30:08 +0100

    Added dberrors to module

--- FA_tools/__init__.py
+++ FA_tools/__init__.py
@@ -1,5 +1,6 @@
 from .search import main as db_search
 from .tools import tiers
+from .repair import dberrors
 from sys import platform
 if platform not in ('win32', 'cygwin'):
     from .signal_handler_unix import sigint_block, sigint_ublock, sigint_check
