a613214177d54d2f7790a3f97e4879c9c591a9a0
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-01-20 23:01:18 +0100

    Completely reworked functions

--- FA_db/repair.py
+++ FA_db/repair.py
@@ -4,6 +4,31 @@ import re
 from FA_dl import session, dl_usr, dl_sub, check_page
 from FA_tools import tiers, sigint_check
 
+def check_values(sub):
+    if None in sub:
+        return False
+    elif '' in (sub[1], sub[2], sub[4], sub[8]):
+        return False
+    elif sub[7] != '0' and sub[6] == '':
+        return False
+    elif sub[8] != tiers(sub[0])+f'/{sub[0]:0>10}':
+        return False
+
+    return True
+
+def check_files(sub):
+    loc = 'FA.files/'+sub[8]
+    if not os.path.isdir(loc):
+        return False
+    elif not os.path.isfile(loc+'/info.txt'):
+        return False
+    elif not os.path.isfile(loc+'/description.html'):
+        return False
+    elif sub[7] != '0' and not os.path.isfile(loc+f'/{sub[7]}'):
+        return False
+
+    return True
+
 def find_errors(DB):
     subs = DB.execute('SELECT * FROM submissions ORDER BY id ASC')
 
@@ -18,31 +43,12 @@ def find_errors(DB):
             errs_id.append(s)
             continue
 
-        err = [s[0]]
-        if None in s:
-            err.append('n')
-        if '' in (s[1], s[2], s[4], s[8]):
-            err.append('e')
-        if s[7] != '0' and s[6] == '':
-            err.append('l')
-        if s[8] != tiers(s[0])+f'/{s[0]:0>10}':
-            err.append('f')
-        if len(err) > 1:
-            errs_vl.append(err)
+        if not check_values(s):
+            errs_vl.append(s)
+            continue
 
-        loc = 'FA.files/'+s[8]
-        if not os.path.isdir(loc):
-            errs_fl.append(s[0])
-            continue
-        if not os.path.isfile(loc+'/info.txt'):
-            errs_fl.append(s[0])
-            continue
-        if not os.path.isfile(loc+'/description.html'):
-            err_fls.append(s[0])
-            continue
-        if s[7] != '0' and not os.path.isfile(loc+f'/{s[7]}'):
-            errs_fl.append(s[0])
-            continue
+        if not check_files(s):
+            errs_fl.append(s)
 
     return errs_id, errs_vl, errs_fl
 
@@ -51,9 +57,9 @@ def dberrors(DB):
     errs_id, errs_vl, errs_fl = find_errors(DB)
     print('Done')
 
-    print(f'There are {len(errs_id)} id errors')
-    print(f'There are {len(errs_vl)} field value errors')
-    print(f'There are {len(errs_fl)} files errors')
+    print(f'Found {len(errs_id)} id error{"s"*bool(len(errs_id) != 1)}')
+    print(f'Found {len(errs_vl)} field values error{"s"*bool(len(errs_vl) != 1)}')
+    print(f'Found {len(errs_fl)} files error{"s"*bool(len(errs_fl) != 1)}')
     print()
 
     if len(errs_id) or len(errs_vl) or len(errs_fl):
@@ -65,42 +71,46 @@ def dberrors(DB):
             return
 
         if len(errs_vl):
-            print('Fixing field values errors')
-            i, l = 0, len(str(len(errs_vl)))
+            i, l, L = 0, len(str(len(errs_vl))), len(errs_vl)
+            errs_fl_mv = 0
             for sub in errs_vl:
                 if sigint_check(): return
-                sub = str(sub[0])
+                ID = sub[0]
                 i += 1
-                print(f'\r{i:0>{l}}/{len(errs_vl)} - {sub:0>10}', end='', flush=True)
-                sub_db = DB.execute(f'SELECT * FROM submissions WHERE id = {int(sub)}').fetchall()[0]
-                if sub_db[3] == None and None not in sub_db[0:3]+sub_db[4:]:
-                    DB.execute(f'UPDATE submissions SET title = "" WHERE id = {int(sub)}')
+                print(f'\rFixing field values errors ... {i:0>{l}}/{L}', end='', flush=True)
+                if sub[3] == None: sub[3] = ''
+                if sub[5] == None: sub[5] = ''
+                if check_values(sub):
+                    DB.execute(f'UPDATE submissions SET title = "{sub[3]}" WHERE id = {ID}')
+                    DB.execute(f'UPDATE submissions SET tags = "{sub[5]}" WHERE id = {ID}')
                     DB.commit()
-                if not check_page(Session, 'view/'+sub):
+                    if not check_files(sub):
+                        errs_fl.append(sub)
+                        errs_fl_mv += 1
+                    continue
+                if not check_page(Session, 'view/'+str(ID)):
                     print(' - Page Error')
                     continue
-                DB.execute(f'DELETE FROM submissions WHERE id = {int(sub)}')
+                DB.execute(f'DELETE FROM submissions WHERE id = {ID}')
                 DB.commit()
-                dl_sub(Session, sub, f'FA.files/{tiers(sub)}/{sub:0>10}', DB, True, False, 2)
+                dl_sub(Session, str(ID), f'FA.files/{tiers(ID)}/{ID:0>10}', DB, True, False, 2)
             print('\r', end=' '*(l+l+1+3+10)+'\r', flush=True)
+            print('Fixing field values errors ... Done')
+            if errs_fl_mv: print(f'{errs_fl_mv} new submission{"s"*bool(len(errs_fl_mv) != 1)} with files missing')
 
         if len(errs_fl):
-            print('Fixing missing files')
-            return
-            i, l = 0, len(str(len(errs_fl)))
+            i, l, L = 0, len(str(len(errs_fl))), len(errs_fl)
             for sub in errs_fl:
                 if sigint_check(): return
-                sub = str(sub)
+                ID = sub[0]
                 i += 1
-                print(f'\r{i:0>{l}}/{len(errs_fl)} - {sub:0>10}', end='', flush=True)
-                if not check_page(Session, 'view/'+sub):
+                print(f'\rFixing missing files ... {i:0>{l}}/{L}', end='', flush=True)
+                if not check_page(Session, 'view/'+str(ID)):
                     print(' - Page Error')
                     continue
-                if int(sub) in [s[0] for s in errs_vl]:
-                    continue
-                loc = DB.execute(f'SELECT location FROM submissions WHERE id = {int(sub)}').fetchall()[0][0]
-                dl_sub(Session, sub, f'FA.files/{loc}', DB, True, False, 2)
+                dl_sub(Session, str(ID), f'FA.files/{sub[8]}', DB, True, False, 2)
             print('\r', end=' '*(l+l+1+3+10)+'\r', flush=True)
+            print('Fixing missing files ... Done')
 
         print()
 
