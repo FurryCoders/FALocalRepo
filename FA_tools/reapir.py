9596d9ffb9f21574900d24e2b4c14d8c5853e182
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-01-20 15:29:58 +0100

    Condensed all checks into a single function

--- FA_tools/reapir.py
+++ FA_tools/reapir.py
@@ -1,25 +1,45 @@
 import sqlite3
 import os
+import re
+from .tools import tiers
 
-def missingfiles(DB):
-    subs = DB.execute('SELECT id, location, filename FROM submissions ORDER BY id ASC')
+def dberrors(DB):
+    subs = DB.execute('SELECT * FROM submissions ORDER BY id ASC')
 
     subs = [[si for si in s] for s in subs.fetchall()]
-    errs = []
+    errs_vl = []
+    errs_id = []
+    errs_fl = []
+    re_id = re.compile('^(|0+)$')
 
     for s in subs:
-        loc = 'FA.files/'+s[1]
-        if not os.path.isdir(loc):
-            errs.append(s[0])
-            continue
-        if not os.path.isfile(loc+'/info.txt'):
-            errs.append(s[0])
-            continue
-        if not os.path.isfile(loc+'/description.html'):
-            errs.append(s[0])
-            continue
-        if s[2] != 0 and not os.path.isfile(loc+f'/{s[2]}'):
-            errs.append(s[0])
+        if re_id.match(str(s[0])):
+            errs_id.append(s)
             continue
 
-    return errs
+        err = [s[0]]
+        if None in s:
+            err.append('n')
+        if '' in (s[1], s[2], s[4], s[6], s[8]):
+            err.append('f')
+        if s[8] != tiers(s[0])+f'/{s[0]:0>10}':
+            err.append('l')
+        if len(err) > 1:
+            errs_vl.append(err)
+            continue
+
+        loc = 'FA.files/'+s[8]
+        if not os.path.isdir(loc):
+            errs_fl.append(s[0])
+            continue
+        if not os.path.isfile(loc+'/info.txt'):
+            errs_fl.append(s[0])
+            continue
+        if not os.path.isfile(loc+'/description.html'):
+            err_fls.append(s[0])
+            continue
+        if s[7] != 0 and not os.path.isfile(loc+f'/{s[7]}'):
+            errs_fl.append(s[0])
+            continue
+
+    return errs_id, errs_vl, errs_fl
