f849f7c9913b9c13de9e40d60a4d08ed4ca3951e
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-02-09 14:35:33 +0100

    Checks that the database is already present and initialized

--- FA_upd/db_update.py
+++ FA_upd/db_update.py
@@ -3,6 +3,12 @@ from .v1v2 import db_update_v1v2
 
 def db_update():
     db = sqlite3.connect('FA.db')
+
+    infos = db.execute('SELECT name FROM sqlite_master WHERE type = "table"').fetchall()
+    if infos != [('SUBMISSIONS',), ('USERS',), ('INFOS',)]:
+        db.close()
+        return
+
     infos_f = db.execute('SELECT field FROM infos').fetchall()
     infos_v = db.execute('SELECT value FROM infos').fetchall()
     db.close()
