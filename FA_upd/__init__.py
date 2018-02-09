40e119cd018d2ffd49bc430d1b3c2c4f40f8f396
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-02-09 15:02:28 +0100

    Changed function names and usage

--- FA_upd/__init__.py
+++ FA_upd/__init__.py
@@ -1,1 +1,1 @@
-from .db_update import db_update
+from .db_update import db_update_main as db_update
--- FA_upd/db_update.py
+++ FA_upd/db_update.py
@@ -1,7 +1,7 @@
 import sqlite3
 from .v1v2 import db_update_v1v2
 
-def db_update():
+def db_update_main():
     db = sqlite3.connect('FA.db')
 
     infos = db.execute('SELECT name FROM sqlite_master WHERE type = "table"').fetchall()
@@ -16,9 +16,16 @@ def db_update():
     infos_f = [f[0] for f in infos_f]
     infos_v = [v[0] for v in infos_v]
 
+    db_update = False
+
     if 'VERSION' not in infos_f:
-        db_update_v1v2()
+        db_update = db_update_v1v2
     else:
         version = infos_v[infos_f.index('VERSION')]
-        if version == '2.0':
-            return
+
+    if db_update:
+        print('-'*20)
+        print('Database version update')
+        print('-'*20)
+        print()
+        db_update()
