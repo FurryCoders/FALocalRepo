8c90b597e1640608fee59408d7751d3fee91d83b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2018-04-20 14:26:23 +0200

    Added function to clear pending SIGINT

--- FA_tools/__init__.py
+++ FA_tools/__init__.py
@@ -1,7 +1,17 @@
 from .search import main as db_search
 from .tools import tiers, cookies_error
+
 from sys import platform
 if platform not in ('win32', 'cygwin'):
     from .signal_handler_unix import sigint_block, sigint_ublock, sigint_check
 else:
     from .signal_handler_windows import sigint_block, sigint_ublock, sigint_check
+
+def sigint_clear():
+    while True:
+        try:
+            sigint_ublock()
+            return
+        except:
+            pass
+    sigint_block()
