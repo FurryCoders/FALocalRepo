b7875c4112b47c672d10ff8e904db0169160788d
Matteo Campinoti <matteo.campinoti94@gmail.com>
2018-04-23 15:15:12 +0200

    Store fields in dictionary

--- FA_db/search.py
+++ FA_db/search.py
@@ -2,6 +2,7 @@ import sqlite3
 import re
 import time, os, sys
 import PythonRead as readkeys
+from FA_tools import sigint_block, sigint_ublock, sigint_check, sigint_clear
 
 def regexp(pattern, input):
     return bool(re.match(pattern, input, flags=re.IGNORECASE))
@@ -10,4 +11,36 @@ def search(DB, fields):
     DB.create_function("REGEXP", 2, regexp)
 
 def main(DB):
-    pass
+    while True:
+        fields = {}
+
+        try:
+            sigint_ublock()
+            fields['user'] = readkeys.input('User: ').strip()
+            if fields['user'] != '':
+                fields['sect'] = readkeys.input('Section: ').strip()
+            else:
+                fields['sect'] = ''
+            fields['titl'] = readkeys.input('Title: ')
+            fields['tags'] = readkeys.input('Tags: ')
+            fields['catg'] = readkeys.input('Category: ')
+            fields['spec'] = readkeys.input('Species: ')
+            fields['gend'] = readkeys.input('Gender: ').lower()
+            fields['ratg'] = readkeys.input('Rating: ').lower()
+        except:
+            return
+        finally:
+            sigint_clear()
+
+        if all(v == '' for v in fields.values()):
+            print('At least one field needs to be used')
+            print()
+            continue
+
+        break
+
+    search(DB, fields)
+    print()
+    print('Press any key to continue ', end='', flush=True)
+    readkeys.getkey()
+    print('\b \b'*26, end='')
