b21fa63537baf0167a3e0ff91ce72a5e16814909
Matteo Campinoti <matteo.campinoti94@gmail.com>
2018-04-25 02:00:44 +0200

    Use tuple of tuples for menu

--- FA.py
+++ FA.py
@@ -13,11 +13,12 @@ def header(s, brk='-', end='\n', l=20):
     print(end=end)
 
 def menu(DB):
-    menu = []
-    menu.append(['Download & Update', fadl.download_main])
-    menu.append(['Search', fadb.db_search])
-    menu.append(['Repair database', fadb.repair])
-    menu.append(['Exit', (lambda *x: sys.exit(0))])
+    menu = (
+        ('Download & Update', fadl.download_main),
+        ('Search', fadb.db_search),
+        ('Repair database', fadb.repair),
+        ('Exit', (lambda *x: sys.exit(0)))
+    )
     menu = {str(k): mk for k, mk in enumerate(menu, 1)}
 
     menu_l = [f'{i}) {menu[i][0]}' for i in menu]
