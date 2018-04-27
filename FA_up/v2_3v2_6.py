2be67d292afd957b07486d3f9851f03f510d8196
Matteo Campinoti <matteo.campinoti94@gmail.com>
2018-04-28 01:33:36 +0200

    Interrupt check at the end of each update cycke

--- FA_up/v2_3v2_6.py
+++ FA_up/v2_3v2_6.py
@@ -151,12 +151,6 @@ def db_upgrade_v2_3v2_6():
     Nl = int(log10(N)+1)
     Ni = 0
     for s in subs_new:
-        if sigint_check():
-            print(' Interrupt')
-            print('Update interrupted, it may be resumed later')
-            print('Closing program')
-            sys.exit(0)
-
         Ni += 1
         print(f'{Ni:0>{Nl}}/{N}', end='', flush=True)
         if s[1]:
@@ -171,6 +165,12 @@ def db_upgrade_v2_3v2_6():
 
         db_new.execute(f"UPDATE submissions SET description = ? WHERE id = {s[0]}", (desc,))
         db_new.commit()
+
+        if sigint_check():
+            print(' Interrupt')
+            print('Update interrupted, it may be resumed later')
+            print('Closing program')
+            sys.exit(0)
 
         print('\b \b'+'\b \b'*(Nl*2), end='', flush=True)
 
