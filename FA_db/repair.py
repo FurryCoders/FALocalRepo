2e9373cd09ac71373b62b98b45eb5f11e422bc60
Matteo Campinoti <matteo.campinoti94@gmail.com>
2018-04-30 14:25:57 +0200

    Add menu to repair section

--- FA_db/repair.py
+++ FA_db/repair.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os, glob
 import re
+import PythonRead as readkeys
 import FA_dl as fadl
 import FA_db as fadb
 import FA_tools as fatl
@@ -152,7 +153,17 @@ def usr_find_errors(db):
 
     return errs_empty, errs_repet, errs_names, errs_namef, errs_foldr, errs_fl_dl
 
-def repair_subs(Session, db, errs_id, errs_vl, errs_fl):
+def repair_subs(Session, db):
+    print('Analyzing submissions database for errors ... ', end='', flush=True)
+    errs_id, errs_vl, errs_fl = sub_find_errors(db)
+    print('Done')
+    print(f'Found {len(errs_id)} id error{"s"*bool(len(errs_id) != 1)}')
+    print(f'Found {len(errs_vl)} field values error{"s"*bool(len(errs_vl) != 1)}')
+    print(f'Found {len(errs_fl)} files error{"s"*bool(len(errs_fl) != 1)}')
+    print()
+
+    fatl.sigint_clear()
+
     if any(len(errs) for errs in (errs_id, errs_vl, errs_fl)):
         Session = fadl.session(Session)
         print()
@@ -232,7 +243,19 @@ def repair_subs(Session, db, errs_id, errs_vl, errs_fl):
 
     return Session
 
-def repair_usrs(Session, db, errs_empty, errs_repet, errs_names, errs_namef, errs_foldr, errs_fl_dl):
+def repair_usrs(Session, db):
+    print('Analyzing users database for errors ... ', end='', flush=True)
+    errs_empty, errs_repet, errs_names, errs_namef, errs_foldr, errs_fl_dl = usr_find_errors(db)
+    print('Done')
+    print(f'Found {len(errs_empty)} empty user{"s"*bool(len(errs_empty) != 1)}')
+    print(f'Found {len(errs_repet)} repeated user{"s"*bool(len(errs_repet) != 1)}')
+    print(f'Found {len(errs_names)} capitalized username{"s"*bool(len(errs_names) != 1)}')
+    print(f'Found {len(errs_foldr)} empty folder{"s"*bool(len(errs_foldr) != 1)}')
+    print(f'Found {len(errs_fl_dl)} empty folder download{"s"*bool(len(errs_fl_dl) != 1)}')
+    print()
+
+    fatl.sigint_clear()
+
     if any(len(errs) for errs in (errs_empty, errs_repet, errs_names, errs_names, errs_foldr, errs_fl_dl)):
         print()
 
@@ -348,45 +371,61 @@ def repair_usrs(Session, db, errs_empty, errs_repet, errs_names, errs_namef, err
 
     return Session
 
-def repair(Session, db):
-    fatl.header('Repair database')
-
-    print('Analyzing submissions database for errors ... ', end='', flush=True)
-    errs_id, errs_vl, errs_fl = sub_find_errors(db)
-    print('Done')
-    print(f'Found {len(errs_id)} id error{"s"*bool(len(errs_id) != 1)}')
-    print(f'Found {len(errs_vl)} field values error{"s"*bool(len(errs_vl) != 1)}')
-    print(f'Found {len(errs_fl)} files error{"s"*bool(len(errs_fl) != 1)}')
-    print()
-
+def repair_all(Session, db):
+    repair_subs(Session, db)
     fatl.sigint_clear()
 
-    print('Analyzing users database for errors ... ', end='', flush=True)
-    errs_empty, errs_repet, errs_names, errs_namef, errs_foldr, errs_fl_dl = usr_find_errors(db)
-    print('Done')
-    print(f'Found {len(errs_empty)} empty user{"s"*bool(len(errs_id) != 1)}')
-    print(f'Found {len(errs_repet)} repeated user{"s"*bool(len(errs_vl) != 1)}')
-    print(f'Found {len(errs_names)} capitalized username{"s"*bool(len(errs_fl) != 1)}')
-    print(f'Found {len(errs_foldr)} empty folder{"s"*bool(len(errs_fl) != 1)}')
-    print(f'Found {len(errs_fl_dl)} empty folder download{"s"*bool(len(errs_fl) != 1)}')
-    print()
-
+    repair_usrs(Session, db)
     fatl.sigint_clear()
 
-    Session = repair_subs(Session, db, errs_id, errs_vl, errs_fl)
+    return Session
 
-    fatl.sigint_clear()
-
-    Session = repair_usrs(Session, db, errs_empty, errs_repet, errs_names, errs_namef, errs_foldr, errs_fl_dl)
-
-    fatl.sigint_clear()
-
+def index(Session, db):
     print('Indexing new entries ... ', end='', flush=True)
     fadb.mkindex(db)
-    print('Done')
+    print('Done\n')
+
+    return Session
+
+def vacuum(Session, db):
     print('Optimizing database ... ', end='', flush=True)
     db.execute("VACUUM")
-    print('Done')
-    print('\nAll done')
+    print('Done\n')
+
+    return Session
+
+def repair(Session, db):
+    menu = (
+        ('Submissions', repair_subs),
+        ('Users', repair_usrs),
+        ('All', repair_all),
+        ('Index', index),
+        ('Optimize', vacuum),
+        ('Return to menu', (lambda *x: x[0]))
+    )
+    menu = {str(k): mk for k, mk in enumerate(menu, 1)}
+    menu_l = [f'{i}) {menu[i][0]}' for i in menu]
+
+    while True:
+        fatl.sigint_clear()
+
+        fatl.header('Repair database')
+
+        print('\n'.join(menu_l))
+        print('\nChoose option: ', end='', flush=True)
+        k = '0'
+        while k not in menu:
+            k = readkeys.getkey()
+            k = k.replace('\x03', str(len(menu)))
+            k = k.replace('\x04', str(len(menu)))
+            k = k.replace('\x1b', str(len(menu)))
+        print(k+'\n')
+
+        if k == str(len(menu)):
+            break
+        print()
+        Session = menu[k][1](Session, db)
+
+        print('-'*30+'\n')
 
     return Session
