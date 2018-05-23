5ab465be587cda0477a90251347c1a939b117846
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-23 15:26:37 +0200

    Fix errors

--- FA_tools/log.py
+++ FA_tools/log.py
@@ -2,9 +2,15 @@ from datetime import datetime
 import FA_var as favar
 
 def log_trim(file=favar.log_file, lines=10000):
-    with open(file, 'r') as log: file = log.readlines()
+    with open(file, 'r') as log:
+        file = log.readlines()
+
+    if len(file) <= lines:
+        return
     file = file[-lines:]
-    with open(file, 'w') as log: log.writelines(file)
+
+    with open(file, 'w') as log:
+        for l in file: log.write(l)
 
 def log(data='', file=favar.log_file):
     with open(file, 'a')as log:
