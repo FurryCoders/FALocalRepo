cc4397e2c225123490114116e57cab0d055e30bb
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-23 21:52:55 +0200

    Add logging to signal handling

--- FA_tools/signal_handler_unix.py
+++ FA_tools/signal_handler_unix.py
@@ -1,18 +1,23 @@
 import signal
+from .log import log
 
 def sigint_block():
+    log('SIGNAL -> sigint block')
     signal.pthread_sigmask(signal.SIG_BLOCK, {signal.SIGINT})
 
 def sigint_ublock():
+    log('SIGNAL -> sigint unblock')
     signal.pthread_sigmask(signal.SIG_UNBLOCK, {signal.SIGINT})
 
 def sigint_check():
     if signal.SIGINT in signal.sigpending():
+        log('SIGNAL -> sigint check:True')
         return True
     else:
         return False
 
 def sigint_clear():
+    log('SIGNAL -> sigint clear')
     while True:
         try:
             sigint_ublock()
