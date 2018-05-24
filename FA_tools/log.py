f0a3414732bfb5094f9c2f48eb0ff4fc3b3b6ce8
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-24 17:09:06 +0200

    Fix variable name error

--- FA_tools/log.py
+++ FA_tools/log.py
@@ -3,15 +3,15 @@ from datetime import datetime
 import FA_var as favar
 
 def log_trim(file=favar.log_file, lines=10000):
-    with open(file, 'r') as log:
-        file = log.readlines()
+    with open(file, 'r') as f:
+        log = f.readlines()
 
-    if len(file) <= lines:
+    if len(log) <= lines:
         return
-    file = file[-lines:]
+    log = log[-lines:]
 
-    with open(file, 'w') as log:
-        for l in file: log.write(l)
+    with open(file, 'w') as f:
+        for l in log: f.write(l)
 
 def log_start(file=favar.log_file):
     with open(file, 'a')as log:
@@ -21,11 +21,11 @@ class log:
     def normal(data='', file=favar.log_file):
         if '--log' not in sys.argv[1:] and '--logv' not in sys.argv[1:]:
             return
-        with open(file, 'a')as log:
+        with open(file, 'a') as log:
             log.write(f'{str(datetime.now())} | {data}\n')
 
     def verbose(data='', file=favar.log_file):
         if '--logv' not in sys.argv[1:]:
             return
-        with open(file, 'a')as log:
+        with open(file, 'a') as log:
             log.write(f'{str(datetime.now())} | {data}\n')
