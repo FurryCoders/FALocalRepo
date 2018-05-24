ea8fc31cc3b7a992e843f0e544eadc74db00d4ab
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-24 17:42:17 +0200

    Remove custom signal handling

--- FA_tools/__init__.py
+++ FA_tools/__init__.py
@@ -1,8 +1,2 @@
 from .tools import *
 from .log import *
-
-from sys import platform
-if platform not in ('win32', 'cygwin'):
-    from .signal_handler_unix import sigint_block, sigint_ublock, sigint_check, sigint_clear
-else:
-    from .signal_handler_windows import sigint_block, sigint_ublock, sigint_check, sigint_clear
