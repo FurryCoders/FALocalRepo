f712fb427a0417b268da6e79fabce70aad0adfc3
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-25 14:15:44 +0200

    Rework log class methods into classmethods, add warning method

--- FA_tools/log.py
+++ FA_tools/log.py
@@ -18,14 +18,25 @@ def log_start(file=favar.log_file):
         log.write('*'*44+'\n')
 
 class log:
-    def normal(data='', file=favar.log_file):
-        if '--log' not in sys.argv[1:] and '--logv' not in sys.argv[1:]:
-            return
-        with open(file, 'a') as log:
-            log.write(f'{str(datetime.now())} | {data}\n')
+    _normal = '--log' in sys.argv[1:] or '--logv' in sys.argv[1:]
+    _verbose = '--logv' in sys.argv[1:]
+    _file = favar.log_file
 
-    def verbose(data='', file=favar.log_file):
-        if '--logv' not in sys.argv[1:]:
+    @classmethod
+    def normal(cls, data=''):
+        if not cls._normal:
             return
-        with open(file, 'a') as log:
-            log.write(f'{str(datetime.now())} | {data}\n')
+        with open(cls._file, 'a') as log:
+            log.write(f'{str(datetime.now())} | N | {data}\n')
+
+    @classmethod
+    def verbose(cls, data=''):
+        if not cls._verbose:
+            return
+        with open(cls._file, 'a') as log:
+            log.write(f'{str(datetime.now())} | V | {data}\n')
+
+    @classmethod
+    def warning(cls, data=''):
+        with open(cls._file, 'a') as log:
+            log.write(f'{str(datetime.now())} | W | {data}\n')
