44d4cfbcbabd674b9d78ae5eafb50fd5c0cb0a74
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-04 00:47:42 +0200

    main - show menu main menu

--- fa/main.py
+++ fa/main.py
@@ -1,6 +1,22 @@
 from typing import Dict
 from typing import List
 
+from faapi import FAAPI
+
+from .menu import menu
+
 
 def main(workdir: str, cookies: List[Dict[str, str]]):
-    pass
+    # Initialise api
+    api: FAAPI = FAAPI(cookies)
+
+    main_menu: List[str] = [
+        "Download",
+        "Database",
+        "Settings",
+        "Exit"
+    ]
+
+    while choice := menu(main_menu):
+        if choice >= len(main_menu) - 1:
+            break
