2ad9434890e0966bd9f16ff3ba9e6e27d32b61cd
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-05 00:47:58 +0200

    interactive - load cookies into FAAPI

--- falocalrepo/interactive.py
+++ falocalrepo/interactive.py
@@ -76,6 +76,7 @@ def settings_menu(api: FAAPI, db: Connection):
 
 def main_menu(workdir: str, db: Connection):
     api: FAAPI = FAAPI()
+    load_cookies(api, *cookies_read(db))
 
     menu_items: List[str] = [
         "Download",
