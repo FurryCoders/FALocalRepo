4d13c6232d9260de9541333c6a5bdce701e8921b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-04 13:53:49 +0200

    settings - move prompts outside of settings and into interactive

--- falocalrepo/interactive.py
+++ falocalrepo/interactive.py
@@ -5,7 +5,7 @@ from faapi import FAAPI
 
 from .database import Connection
 from .menu import menu
-from .settings import change_cookies
+from .settings import cookies_change, cookies_load
 
 
 def download_menu(api: FAAPI, db: Connection):
@@ -38,8 +38,17 @@ def settings_menu(api: FAAPI, db: Connection):
         if choice == len(menu_items):
             break
         elif choice == 1:
-            cookies_new: Dict[str, str] = change_cookies(db)
-            api.load_cookies([{"name": k, "value": v} for k, v in cookies_new.items()])
+            print("Insert new values for cookies 'a' and 'b'.")
+            print("Leave empty to keep previous value.\n")
+
+            cookie_a_old, cookie_b_old = cookies_load(db)
+
+            cookie_a: str = input(f"[{cookie_a_old}]\na: ")
+            cookie_b: str = input(f"[{cookie_b_old}]\nb: ")
+
+            if cookie_a or cookie_b:
+                cookies_change(db, cookie_a, cookie_b)
+                api.load_cookies([{"name": "a", "value": cookie_a}, {"name": "b", "value": cookie_b}])
 
 
 def main_menu(workdir: str, api: FAAPI, db: Connection):
--- falocalrepo/settings.py
+++ falocalrepo/settings.py
@@ -1,28 +1,17 @@
 from json import dumps as json_dumps
 from json import loads as json_loads
 from typing import Dict
+from typing import Tuple
 
 from .database import Connection
 from .database import setting_read
 from .database import setting_write
 
 
-def change_cookies(db: Connection) -> Dict[str, str]:
-    print("Insert new values for cookies 'a' and 'b'.")
-    print("Leave empty to keep previous value.\n")
-
+def cookies_load(db: Connection) -> Tuple[str, str]:
     cookies: Dict[str, str] = json_loads(setting_read(db, "COOKIES"))
+    return cookies.get("a", ""), cookies.get("b", "")
 
-    cookie_a_old: str = cookies.get('a', '')
-    cookie_b_old: str = cookies.get('b', '')
 
-    cookie_a_new: str = input(f"[{cookie_a_old}]\na: ")
-    cookie_b_new: str = input(f"[{cookie_b_old}]\nb: ")
-
-    cookies["a"] = cookie_a_new if cookie_a_new else cookie_a_old
-    cookies["b"] = cookie_b_new if cookie_b_new else cookie_b_old
-
-    if cookie_a_new or cookie_b_new:
-        setting_write(db, "COOKIES", json_dumps(cookies))
-
-    return cookies
+def cookies_change(db: Connection, a: str, b: str):
+    setting_write(db, "COOKIES", json_dumps({"a": a, "b": b}))
