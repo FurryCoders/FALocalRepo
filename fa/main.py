678f5fa52ee2a60ad8299b6a15ff7b57a439b268
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-04 02:40:17 +0200

    main - add submenus for download, database and settings

--- fa/main.py
+++ fa/main.py
@@ -8,6 +8,36 @@ from .database import connect_database
 from .menu import menu
 
 
+def download(api: FAAPI, db: Connection):
+    dl_menu: List[str] = [
+        "Exit",
+    ]
+
+    while choice := menu(dl_menu):
+        if choice == len(dl_menu):
+            break
+
+
+def database(db: Connection):
+    db_menu: List[str] = [
+        "Exit",
+    ]
+
+    while choice := menu(db_menu):
+        if choice == len(db_menu):
+            break
+
+
+def settings(db: Connection):
+    s_menu: List[str] = [
+        "Exit",
+    ]
+
+    while choice := menu(s_menu):
+        if choice == len(s_menu):
+            break
+
+
 def main(workdir: str, cookies: List[Dict[str, str]]):
     # Initialise api and database
     api: FAAPI = FAAPI(cookies)
@@ -23,3 +53,9 @@ def main(workdir: str, cookies: List[Dict[str, str]]):
     while choice := menu(main_menu):
         if choice == len(main_menu):
             break
+        elif choice == 1:
+            download(api, db)
+        elif choice == 2:
+            database(db)
+        elif choice == 3:
+            settings(db)
