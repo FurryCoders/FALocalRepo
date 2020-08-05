bc63fb1f7589003010a3bbe819fc292e12316f95
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-05 14:58:36 +0200

    main - update version

--- falocalrepo/main.py
+++ falocalrepo/main.py
@@ -4,6 +4,7 @@ from os.path import abspath
 from os.path import join as path_join
 from sys import argv
 
+from .__version__ import __version__
 from .console import main_console
 from .database import Connection
 from .database import connect_database
@@ -20,6 +21,7 @@ def main():
     db: Connection = connect_database(path_join(workdir, "FA.db"))
     make_database(db)
     setting_write(db, "LASTSTART", str(datetime.now().timestamp()))
+    setting_write(db, "VERSION", __version__)
 
     # Run main program
     if argv[1:] and argv[1] == "interactive":
