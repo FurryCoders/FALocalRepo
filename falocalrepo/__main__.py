aa0b1de1c8b7f1cfabe7c4e6c10c1c32813ef0ff
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-07 23:07:59 +0200

    main - remove and move console caller to __main__

--- falocalrepo/__main__.py
+++ falocalrepo/__main__.py
@@ -1,3 +1,19 @@
+from sys import argv
+
+from .console import main_console
+
+
+def main():
+    # Run main program
+    try:
+        main_console(argv)
+    except KeyboardInterrupt:
+        print()
+        pass
+    except (Exception, BaseException) as err:
+        print("\nERROR:", repr(err))
+
+
 if __name__ == "__main__":
     from .main import main
 
