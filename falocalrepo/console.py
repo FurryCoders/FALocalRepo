0a03dfc7417af055dfd78340ccdc2d6bee7bd2a1
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-16 16:45:00 +0200

    console - separate variable to hold sub-commands, fix missing exceptions

--- falocalrepo/console.py
+++ falocalrepo/console.py
@@ -42,75 +42,85 @@ class CommandError(Exception):
 
 
 def config(db: Connection, args: List[str]):
-    if not args:
+    comm: str = args[0] if args else ""
+    args = args[1:]
+
+    if not comm:
         cookie_a, cookie_b = cookies_read(db)
         folder: str = setting_read(db, "FILESFOLDER")
         print("cookie a:", cookie_a)
         print("cookie b:", cookie_b)
         print("folder  :", folder)
-    elif args[0] == "cookies":
-        if not args[1:]:
+    elif comm == "cookies":
+        if not args:
             cookie_a, cookie_b = cookies_read(db)
             print("cookie a:", cookie_a)
             print("cookie b:", cookie_b)
-        elif len(args[1:]) == 2 and args[1] and args[2]:
-            cookies_write(db, args[1], args[2])
+        elif len(args) == 2 and args[0] and args[1]:
+            cookies_write(db, args[0], args[1])
         else:
             raise CommandError("Malformed command: cookies needs two arguments")
-    elif args[0] == "files-folder":
-        if not args[1:]:
+    elif comm == "files-folder":
+        if not args:
             print("files folder:", setting_read(db, "FILESFOLDER"))
-        elif len(args[1:]) == 1 and args[1]:
-            files_folder_move(db, setting_read(db, "FILESFOLDER"), args[1])
+        elif len(args) == 1 and args[0]:
+            files_folder_move(db, setting_read(db, "FILESFOLDER"), args[0])
         else:
             raise CommandError("Malformed command: files-folder needs one argument")
     else:
-        raise CommandError(f"Unknown config command {args[0]}")
+        raise CommandError(f"Unknown config command {comm}")
 
 
 def download(db: Connection, args: List[str]):
+    comm: str = args[0] if args else ""
+    args = args[1:]
+
+    if not comm:
+        raise CommandError("Malformed command: download needs a command")
+
     api: FAAPI = FAAPI()
     cookies_load(api, *cookies_read(db))
 
-    if not args:
-        raise CommandError("Malformed command: download needs a command")
-    elif args[0] == "update":
+    if comm == "update":
         users: Optional[List[str]] = None
         folders: Optional[List[str]] = None
-        if args[1:] and args[1] != "--":
-            users_tmp: List[str] = list(filter(bool, map(user_clean_name, args[1].split(","))))
+        if args and args[0] != "--":
+            users_tmp: List[str] = list(filter(bool, map(user_clean_name, args[0].split(","))))
             users = sorted(set(users_tmp), key=users_tmp.index)
-        if args[2:] and args[2] != "--":
-            folders_tmp: List[str] = list(filter(bool, map(str.strip, args[2].split(","))))
+        if args[1:] and args[1] != "--":
+            folders_tmp: List[str] = list(filter(bool, map(str.strip, args[1].split(","))))
             folders = sorted(set(folders_tmp), key=folders_tmp.index)
         users_update(api, db, users, folders)
-    elif args[0] == "users":
-        if len(args[1:]) == 2 and args[1] and args[2]:
-            users_tmp: List[str] = list(filter(bool, map(user_clean_name, args[1].split(","))))
+    elif comm == "users":
+        if len(args) == 2 and args[0] and args[1]:
+            users_tmp: List[str] = list(filter(bool, map(user_clean_name, args[0].split(","))))
             users: List[str] = sorted(set(users_tmp), key=users_tmp.index)
-            folders_tmp: List[str] = list(filter(bool, map(str.strip, args[2].split(","))))
+            folders_tmp: List[str] = list(filter(bool, map(str.strip, args[1].split(","))))
             folders: List[str] = sorted(set(folders_tmp), key=folders_tmp.index)
             users_download(api, db, users, folders)
         else:
             raise CommandError("Malformed command: users needs two arguments")
-    elif args[0] == "submissions":
-        if not args[1:]:
+    elif comm == "submissions":
+        if not args:
             raise CommandError("Malformed command: submissions needs at least one argument")
-        sub_ids_tmp: List[str] = list(filter(str.isdigit, args[1:]))
+        sub_ids_tmp: List[str] = list(filter(str.isdigit, args))
         sub_ids: List[str] = sorted(set(sub_ids_tmp), key=sub_ids_tmp.index)
         submissions_download(api, db, sub_ids)
-    elif args[0] == "journals":
-        if not args[1:]:
+    elif comm == "journals":
+        if not args:
             raise CommandError("Malformed command: journals needs at least one argument")
-        journal_ids_tmp: List[str] = list(filter(str.isdigit, args[1:]))
+        journal_ids_tmp: List[str] = list(filter(str.isdigit, args))
         journal_ids: List[str] = sorted(set(journal_ids_tmp), key=journal_ids_tmp.index)
         submissions_download(api, db, journal_ids)
     else:
-        raise CommandError(f"Unknown download command {args[0]}")
+        raise CommandError(f"Unknown download command {comm}")
 
 
 def database(db: Connection, args: List[str]):
-    if not args:
+    comm: str = args[0] if args else ""
+    args = args[1:]
+
+    if not comm:
         sub_n: int = int(setting_read(db, "SUBN"))
         usr_n: int = int(setting_read(db, "USRN"))
         last_update: float = float(setting_read(db, "LASTUPDATE"))
@@ -119,50 +129,50 @@ def database(db: Connection, args: List[str]):
         print("Users      :", usr_n)
         print("Last update:", str(datetime.fromtimestamp(last_update)) if last_update else 0)
         print("Version    :", version)
-    elif args[0] == "search-submissions":
+    elif comm == "search-submissions":
         search_params: Dict[str, List[str]] = {}
-        for param, value in map(lambda p: p.split("=", 1), args[1:]):
+        for param, value in map(lambda p: p.split("=", 1), args):
             param = param.strip().lower()
             search_params[param] = search_params.get(param, []) + [value]
         results: List[tuple] = submissions_search(db, **search_params)
         submissions_print(results, sort=True)
         print(f"Found {len(results)} results")
-    elif args[0] == "search-journals":
+    elif comm == "search-journals":
         search_params: Dict[str, List[str]] = {}
-        for param, value in map(lambda p: p.split("=", 1), args[1:]):
+        for param, value in map(lambda p: p.split("=", 1), args):
             param = param.strip().lower()
             search_params[param] = search_params.get(param, []) + [value]
         results: List[tuple] = journals_search(db, **search_params)
         journals_print(results, sort=True)
         print(f"Found {len(results)} results")
-    elif args[0] == "add-submission":
-        make_params: Dict[str, str] = {(p := arg.split("="))[0].lower(): p[1].strip() for arg in args[1:]}
+    elif comm == "add-submission":
+        make_params: Dict[str, str] = {(p := arg.split("="))[0].lower(): p[1].strip() for arg in args}
         make_params["id_"] = make_params.get("id", "")
         if "id" in make_params:
             del make_params["id"]
         submission_save(db, *submission_make(**make_params))
-    elif args[0] == "add-journal":
-        make_params: Dict[str, str] = {(p := arg.split("="))[0].lower(): p[1].strip() for arg in args[1:]}
+    elif comm == "add-journal":
+        make_params: Dict[str, str] = {(p := arg.split("="))[0].lower(): p[1].strip() for arg in args}
         make_params["id_"] = make_params.get("id", "")
         if "id" in make_params:
             del make_params["id"]
         journal_save(db, journal_make(**make_params))
-    elif args[0] == "remove-users":
-        for user in map(user_clean_name, args[1:]):
+    elif comm == "remove-users":
+        for user in map(user_clean_name, args):
             print("Deleting", user)
             delete(db, "USERS", "USERNAME", user)
             db.commit()
-    elif args[0] == "remove-submissions":
-        for sub in args[1:]:
+    elif comm == "remove-submissions":
+        for sub in args:
             print("Deleting", sub)
             delete(db, "SUBMISSIONS", "ID", int(sub))
             db.commit()
-    elif args[0] == "remove-journals":
-        for sub in args[1:]:
-            print("Deleting", sub)
-            delete(db, "JOURNALS", "ID", int(sub))
+    elif comm == "remove-journals":
+        for jrn in args:
+            print("Deleting", jrn)
+            delete(db, "JOURNALS", "ID", int(jrn))
             db.commit()
-    elif args[0] == "check-errors":
+    elif comm == "check-errors":
         print("Checking submissions table for errors... ", end="", flush=True)
         results: List[tuple] = check_errors(db, "SUBMISSIONS")
         print("Done")
@@ -173,8 +183,10 @@ def database(db: Connection, args: List[str]):
         print("Done")
         if results:
             journals_print(results)
-    elif args[0] == "clean":
+    elif comm == "clean":
         vacuum(db)
+    else:
+        raise CommandError(f"Unknown download command {comm}")
 
 
 def main_console(args: List[str]):
