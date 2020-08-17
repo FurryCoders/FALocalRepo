25a5fc5e5711e9a247d59d3c4a711b9860823b61
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-17 10:25:53 +0200

    database - simplify tiered_path()

--- falocalrepo/database.py
+++ falocalrepo/database.py
@@ -72,11 +72,7 @@ def tiered_path(id_: Union[int, str], tiers: int = 5, depth: int = 2) -> str:
     assert isinstance(depth, int) and depth > 0
 
     id_str: str = str(int(id_)).zfill(tiers * depth)
-    id_tiered: List[str] = []
-    for n in range(0, tiers * depth, depth):
-        id_tiered.append(id_str[n:n + depth])
-
-    return path_join(*id_tiered)
+    return path_join(*[id_str[n:n+depth] for n in range(0, tiers * depth, depth)])
 
 
 def connect_database(db_name: str) -> Connection:
