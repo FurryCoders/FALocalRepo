3a5625c3fd5108a34c969ca4d53edfc9a75fdb02
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-03-26 20:54:50 +0100

    __main__ - do not print exception repr when printing traceback

--- falocalrepo/__main__.py
+++ falocalrepo/__main__.py
@@ -22,34 +22,27 @@ def main():
         print()
         exit(130)
     except (MalformedCommand, UnknownCommand) as err:
-        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else None
-        print(repr(err), file=stderr)
+        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else print(repr(err), file=stderr)
         exit(1)
     except MultipleInstances as err:
-        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else None
-        print(repr(err), file=stderr)
+        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else print(repr(err), file=stderr)
         exit(2)
     except UnknownFolder as err:
-        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else None
-        print(repr(err), file=stderr)
+        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else print(repr(err), file=stderr)
         exit(3)
     except ConnectionError as err:
-        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else None
-        print(repr(err), file=stderr)
+        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else print(repr(err), file=stderr)
         exit(4)
     except (DatabaseError, IntegrityError) as err:
-        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else None
-        print(repr(err), file=stderr)
+        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else print(repr(err), file=stderr)
         exit(5)
     except (TypeError, AssertionError) as err:
-        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else None
-        print(repr(err), file=stderr)
+        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else print(repr(err), file=stderr)
         exit(6)
     except (Exception, BaseException) as err:
-        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else None
+        print_exc(file=stderr) if environ.get("FALOCALREPO_DEBUG", None) is not None else print(repr(err), file=stderr)
         with open(join(getcwd(), "FA.log"), "w") as f:
             print_exc(file=f)
-            print(repr(err), file=stderr)
             print(f"Trace written to {f.name}", file=stderr)
         exit(7)
 
