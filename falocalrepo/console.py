59c1899d9a4f853e8fab2ebd70af1e5d12dbac7f
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-04-22 00:47:18 +0200

    console - fix database not closing properly when running server

--- falocalrepo/console.py
+++ falocalrepo/console.py
@@ -715,8 +715,9 @@ def database_server(db: FADatabase, *args: str):
     """
 
     db.close()
+    database_path, db = db.database_path, None
     opts, _ = parse_args(args)
-    server(db.database_path, host=opts.get("host", "0.0.0.0"), port=int(opts.get("port", 8080)))
+    server(database_path, host=opts.get("host", "0.0.0.0"), port=int(opts.get("port", 8080)))
 
 
 def database_merge_copy(db: FADatabase, merge: bool = True, *args):
