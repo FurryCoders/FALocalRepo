5c0b1bd834e47b4430a066cfb7ada082c4511a18
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-03-04 15:27:14 +0100

    app - remove both leading and trailing newlines from paw

--- falocalrepo/console/app.py
+++ falocalrepo/console/app.py
@@ -393,7 +393,7 @@ def paw(ctx: Context, flag: str, truecolor: bool):
                   =@@@@@@@@@@.  .-++++-.          
                    .#@@@@@@@*                     
                      -*%@@#=                      
-    """.removeprefix("\n").removesuffix("\n")
+    """.strip("\n").rstrip()
 
     paw_ascii = "\n".join(line.ljust(46) for line in paw_ascii.splitlines())
     paw_ascii = "\n".join(bold + c[truecolor] + l + reset for c, l in zip(_pride_colors[flag], paw_ascii.splitlines()))
