72191d244ac0630efb735d5776be6eb7e580525c
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-03-18 23:40:16 +0100

    downloader - do not change stored retry value, remove debug value

--- falocalrepo/downloader.py
+++ falocalrepo/downloader.py
@@ -149,7 +149,7 @@ class Downloader:
         self.db: Database = db
         self.output: OutputType = OutputType.rich if terminal_width() > 0 else OutputType.simple
         self.color: bool = color
-        self.retry: int = retry + 1
+        self.retry: int = retry
         self.dry_run: bool = dry_run
         self.api: FAAPI = api
         self.bar_width: int = 10
@@ -316,9 +316,9 @@ class Downloader:
         self.bar_close("\b")
         self.bar(7)
         file: bytes | None = None
-        retry: int = self.retry + 1
+        retry: int = self.retry + 2
         while file is None and (retry := retry - 1):
-            file = None  # self.download_bytes(submission.file_url)
+            file = self.download_bytes(submission.file_url)
             if file is None and retry:
                 self.bar_message("RETRY")
                 self.api.handle_delay()
@@ -326,7 +326,7 @@ class Downloader:
         self.bar_close("]")
         self.bar(1)
         thumb: bytes | None = None
-        retry = self.retry + 1
+        retry = self.retry + 2
         while thumb is None and (retry := retry - 1):
             thumb = self.download_bytes(submission.thumbnail_url or thumbnail)
             if thumb is None and retry:
