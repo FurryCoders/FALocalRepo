df8fdd6c8484619d88700b400e36f62fc7889057
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-06-08 17:11:42 +0200

    download - add newline before backup

--- falocalrepo/console/download.py
+++ falocalrepo/console/download.py
@@ -177,12 +177,15 @@ def download_users(ctx: Context, database: Callable[..., Database], users: tuple
     folders = folders_
     try:
         downloader.download_users(list(users), list(folders))
+        echo()
         backup_database(db, ctx, "download")
     except Unauthorized as err:
         secho(f"\nError: Unauthorized{(': ' + ' '.join(err.args)) if err.args else ''}", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     except RequestException as err:
         secho(f"\nError: An error occurred during download: {err!r}.", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     finally:
         echo()
@@ -246,12 +249,15 @@ def download_update(ctx: Context, database: Callable[..., Database], users: tupl
         add_history(db, ctx, users=users, folders=folders, stop=stop)
     try:
         downloader.download_users_update(list(users), list(folders), stop, deactivated, like)
+        echo()
         backup_database(db, ctx, "download")
     except Unauthorized as err:
         secho(f"\nError: Unauthorized{(': ' + ' '.join(err.args)) if err.args else ''}", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     except RequestException as err:
         secho(f"\nError: An error occurred during download: {err!r}.", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     finally:
         echo()
@@ -299,12 +305,15 @@ def download_submissions(ctx: Context, database: Callable[..., Database], submis
         add_history(db, ctx, submission_id=submission_id, replace=replace)
     try:
         downloader.download_submissions(list(submission_id))
+        echo()
         backup_database(db, ctx, "download")
     except Unauthorized as err:
         secho(f"\nError: Unauthorized{(': ' + ' '.join(err.args)) if err.args else ''}", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     except RequestException as err:
         secho(f"\nError: An error occurred during download: {err!r}.", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     finally:
         echo()
@@ -348,12 +357,15 @@ def download_journals(ctx: Context, database: Callable[..., Database], journal_i
         add_history(db, ctx, journal_id=journal_id, replace=replace)
     try:
         downloader.download_journals(list(journal_id))
+        echo()
         backup_database(db, ctx, "download")
     except Unauthorized as err:
         secho(f"\nError: Unauthorized{(': ' + ' '.join(err.args)) if err.args else ''}", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     except RequestException as err:
         secho(f"\nError: An error occurred during download: {err!r}.", fg="red", color=ctx.color)
+        echo()
         backup_database(db, ctx, "download")
     finally:
         echo()
