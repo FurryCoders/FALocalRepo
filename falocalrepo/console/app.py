f098941019c5bea0a975f81ed62e32c14da238c5
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-08-29 00:21:55 +0200

    app:pride_colors - add ukrainian flag

--- falocalrepo/console/app.py
+++ falocalrepo/console/app.py
@@ -149,7 +149,11 @@ _pride_colors: dict[str, list[tuple[str, str]]] = {
         *([(dim, "\x1b[38;2;16;16;16m")] * 7),
         *([(bright_white, "\x1b[38;2;255;255;221m")] * 5),
         *([(green, "\x1b[38;2;168;212;122m")] * 7),
-    ]
+    ],
+    "ukraine": [
+        *([(bright_blue, "\x1b[38;2;0;0;255m")] * 9),
+        *([(bright_yellow, "\x1b[38;2;255;215;0m")] * 10),
+    ],
 }
 
 
